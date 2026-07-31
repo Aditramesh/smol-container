42510635f2e8826143a582b24a33f5e4e660dae2
Aditramesh <aditya.raamesh@gmail.com>
2026-07-31 17:48:41 +0530

    use tarball, cgroups not working yet

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,18 @@
 # Build stage
 FROM golang:1.26.2 AS builder
 
-WORKDIR /app
+WORKDIR /src 
 
 COPY go.mod ./
 RUN go mod download
 
-COPY . .
+RUN mkdir ubuntu-fs && \
+    curl -L https://cdimage.ubuntu.com/ubuntu-base/releases/24.04/release/ubuntu-base-24.04.3-base-arm64.tar.gz \
+    | tar -xz -C ubuntu-fs
+
+COPY . ./
+
+RUN mkdir ubuntu-fs/ROOT_FOR_CONTAINER
 
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=arm64 \
     go build -o app .
@@ -14,8 +20,7 @@ RUN CGO_ENABLED=0 GOOS=linux GOARCH=arm64 \
 # Runtime stage
 FROM golang:1.26.2
 
-WORKDIR /app
+COPY --from=builder /src/app /app
+COPY --from=builder /src/ubuntu-fs /ubuntu-fs
 
-COPY --from=builder /app/app .
-
-ENTRYPOINT ["./app"]
+ENTRYPOINT ["/app"]
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"os/user"
 	"syscall"
 )
 
@@ -13,23 +14,58 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("bad command")
 	}
 }
 
 func run() {
-	fmt.Printf("Running %v\n", os.Args[1:])
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+	cmd := exec.Command("../proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
 	}
 	err := cmd.Run()
 	if err != nil {
 		fmt.Println(err)
 	}
-
 }
+
+func child() {
+	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+	u, _ := user.Current()
+	fmt.Println(u.Username)
+	// cgroups()
+	syscall.Sethostname([]byte("container"))
+	syscall.Chroot("/ubuntu-fs")
+	syscall.Chdir("/")
+	syscall.Mount("proc", "proc", "proc", 0, "")
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+	syscall.Unmount("/proc", 0)
+}
+
+// func cgroups() {
+// 	cgroups := "/sys/fs/cgroup"
+// 	err := os.Mkdir(filepath.Join(cgroups, "adi"), 0755)
+// 	if err != nil && !os.IsExist(err) {
+// 		panic(err)
+// 	}
+// 	must(os.WriteFile(filepath.Join(cgroups, "adi/pids.max"), []byte("20"), 0700))
+// 	must(os.WriteFile(filepath.Join(cgroups, "adi/notify_on_release"), []byte("1"), 0700))
+// }
+
+// func must(err error) {
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
